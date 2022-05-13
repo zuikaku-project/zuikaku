fadb18383dd6d0d034b76600aeee5ccda32d868a
Orchit07 <orchitiadi@gmail.com>
2022-05-13 21:16:29 +0700

    chore(deps): update discord.js

--- src/Structures/CommandContext.ts
+++ src/Structures/CommandContext.ts
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unnecessary-condition */
 import { MessageInteractionAction } from "@zuikaku/types/core";
-import { InteractionTypes, MessageComponentTypes } from "@zuikaku/types/enum";
 import { Utils } from "@zuikaku/Utils";
 import {
     ButtonInteraction,
@@ -13,6 +12,8 @@ import {
     GuildMember,
     Interaction,
     InteractionReplyOptions,
+    Guild,
+    InteractionType,
     Message,
     MessageActionRow,
     MessageAttachment,
@@ -27,9 +28,6 @@ import {
 import { ZuikakuClient } from "./ZuikakuClient";
 
 export class CommandContext {
-    public additionalArgs: Collection<string, any> = new Collection();
-    public channel: TextBasedChannel | null = this.context.channel;
-    public guild = this.context.guild;
     public activateCollector = false;
     public constructor(
         public client: ZuikakuClient,
@@ -42,9 +40,85 @@ export class CommandContext {
         public args: string[] = []
     ) {}
 
-    public setAdditionalArgs(key: string, value: any): this {
-        this.additionalArgs.set(key, value);
-        return this;
+    public get id(): string {
+        return this.context.id;
+    }
+
+    public get channel(): TextBasedChannel | null {
+        return this.context.channel;
+    }
+
+    public get author(): User {
+        return this.context instanceof Interaction
+            ? this.context.user
+            : this.context.author;
+    }
+
+    public get guild(): Guild | null {
+        return this.context.guild;
+    }
+
+    public get createdAt(): Date {
+        return this.context.createdAt;
+    }
+
+    public get member(): GuildMember {
+        return this.context.member as GuildMember;
+    }
+
+    public get guildId(): string | null {
+        return this.context.guildId;
+    }
+
+    public get channelId(): string | null {
+        return this.context.channelId;
+    }
+
+    public get type(): InteractionType {
+        return this.context.type as InteractionType;
+    }
+
+    public get attachments(): Collection<string, MessageAttachment> | null {
+        return this.context instanceof Message
+            ? this.context.attachments
+            : null;
+    }
+
+    public get deferred(): boolean {
+        return this.context instanceof Interaction
+            ? (this.context as CommandInteraction).deferred
+            : false;
+    }
+
+    public get options(): CommandInteractionOptionResolver | null {
+        return this.context instanceof Interaction
+            ? ((this.context as CommandInteraction)
+                  .options as CommandInteractionOptionResolver)
+            : null;
+    }
+
+    public get mentions(): MessageMentions | null {
+        return this.context instanceof Message ? this.context.mentions : null;
+    }
+
+    public isInteraction(): boolean {
+        return this.context instanceof Interaction;
+    }
+
+    public isCommand(): boolean {
+        return this.context instanceof CommandInteraction;
+    }
+
+    public isContextMenu(): boolean {
+        return this.context instanceof ContextMenuInteraction;
+    }
+
+    public isButton(): boolean {
+        return this.context instanceof ButtonInteraction;
+    }
+
+    public isSelectMenu(): boolean {
+        return this.context instanceof SelectMenuInteraction;
     }
 
     public async deferReply(ephemeral = false): Promise<void> {
@@ -85,13 +159,14 @@ export class CommandContext {
                 (options as InteractionReplyOptions).components = [row];
             }
         }
-        if (this.isInteraction()) {
+        if (this.isCommand()) {
             (options as InteractionReplyOptions).fetchReply = true;
             const msg = (await (this.context as CommandInteraction)[type](
                 options as any
             )) as Message;
-            const channel = this.context.channel;
-            const res = await channel!.messages.fetch(msg.id).catch(() => null);
+            const res = await this.channel!.messages.fetch(msg.id).catch(
+                () => null
+            );
             return res ?? msg;
         }
         if ((options as InteractionReplyOptions).ephemeral) {
@@ -99,7 +174,7 @@ export class CommandContext {
                 "Cannot send ephemeral message in a non-interaction context"
             );
         }
-        return this.context.channel!.send(options as any);
+        return this.channel!.send(options as any);
     }
 
     public async react(emojis: string[] | string): Promise<void> {
@@ -110,91 +185,5 @@ export class CommandContext {
         } else {
             await (this.context as Message).react(emojis);
         }
-    }
-
-    public get attachments(): Collection<string, MessageAttachment> | null {
-        return this.context instanceof Message
-            ? this.context.attachments
-            : null;
-    }
-
-    public get deferred(): boolean {
-        return this.context instanceof Interaction
-            ? (this.context as CommandInteraction).deferred
-            : false;
-    }
-
-    public get options(): CommandInteractionOptionResolver | null {
-        return this.context instanceof Interaction
-            ? ((this.context as CommandInteraction)
-                  .options as CommandInteractionOptionResolver)
-            : null;
-    }
-
-    public get author(): User {
-        return this.context instanceof Interaction
-            ? this.context.user
-            : this.context.author;
-    }
-
-    public get member(): GuildMember | null {
-        return this.guild!.members.resolve(this.author.id);
-    }
-
-    public get mentions(): MessageMentions | null {
-        return this.context instanceof Message ? this.context.mentions : null;
-    }
-
-    public isInteraction(): boolean {
-        return (
-            this.isCommand() ||
-            this.isContextMenu() ||
-            this.isMessageComponent() ||
-            this.isButton() ||
-            this.isSelectMenu()
-        );
-    }
-
-    public isCommand(): boolean {
-        return (
-            InteractionTypes[(this.context as Interaction).type] ===
-                InteractionTypes.APPLICATION_COMMAND &&
-            typeof (this.context as any).targetId === "undefined"
-        );
-    }
-
-    public isContextMenu(): boolean {
-        return (
-            InteractionTypes[(this.context as Interaction).type] ===
-                InteractionTypes.APPLICATION_COMMAND &&
-            typeof (this.context as any).targetId !== "undefined"
-        );
-    }
-
-    public isMessageComponent(): boolean {
-        return (
-            InteractionTypes[(this.context as Interaction).type] ===
-            InteractionTypes.MESSAGE_COMPONENT
-        );
-    }
-
-    public isButton(): boolean {
-        return (
-            InteractionTypes[(this.context as Interaction).type] ===
-                InteractionTypes.MESSAGE_COMPONENT &&
-            MessageComponentTypes[
-                (this.context as ButtonInteraction).componentType
-            ] === MessageComponentTypes.BUTTON
-        );
-    }
-
-    public isSelectMenu(): boolean {
-        return (
-            InteractionTypes[(this.context as Interaction).type] ===
-                InteractionTypes.MESSAGE_COMPONENT &&
-            MessageComponentTypes[
-                (this.context as SelectMenuInteraction).componentType
-            ] === MessageComponentTypes.SELECT_MENU
-        );
     }
 }
